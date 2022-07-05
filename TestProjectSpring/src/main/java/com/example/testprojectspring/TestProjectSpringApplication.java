2b058da97d71426a4a48b97a5af86066cdb329e4
Vladimir Andreev <92325644+Renser007@users.noreply.github.com>
2022-07-05 12:19:27 +0300

    Initial commit

---
+++ TestProjectSpring/src/main/java/com/example/testprojectspring/TestProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectSpringApplication.class, args);
+    }
+
+}
